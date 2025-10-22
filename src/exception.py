3a2534ffb9b89348908c8f3fd18bdf370544a8b2
Om Thakur <live.onkar@gmail.com>
2025-10-22 01:40:39 -0400

    updating the exception code

--- src/exception.py
+++ src/exception.py
@@ -1,17 +1,8 @@
 import sys
-# creating a custom exception class
-# this class is used to raise an exception with a detailed error message
-# it is used to handle errors in the code
-# it is used to display the error message in a readable format
-# it is used to log the error message
-# it is used to send the error message to the user
-# it is used to send the error message to the developer
-# it is used to send the error message to the administrator
-# it is used to send the error message to the system
-# it is used to send the error message to the network
-# it is used to send the error message to the database
-def error_message_detail(error, error_detail:sys):
-    _,_,exc_tb = error_detail.exc_info()
+from src.logged.logger import logging
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
     # this exc_tb have the information about the error
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
@@ -20,6 +11,16 @@ def error_message_detail(error, error_detail:sys):
     return error_message
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail:sys):
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
+
+if __name__ == "__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Division by zero")
+        raise CustomException(e, sys)  # Pass sys, not sys.exc_info()
